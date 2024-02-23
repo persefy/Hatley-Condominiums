3cd1708247783b383192e7f8a08b22f5126410cc
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-02-23 05:22:48 -0500

    updates include sorting menu on units page

--- client/js/script.js
+++ client/js/script.js
@@ -2,13 +2,16 @@
 const navBtn = document.querySelector('#navBtn');
 const navBar = document.querySelector('#navBar');
 
+const sortBtn = document.querySelector('#unitSort');
+const sortBar = document.querySelector('#unitSorting')
+
 //Available Unit page
 const unitDisplayHolder = document.querySelector('#unitDisplayHolder');
 
 
 
+
 //Populate Data
-//#unitDisplayHolder should display each unit marked as available
 const populateUnits = async () => {
     console.log('JS is working')
     
@@ -19,6 +22,12 @@ const populateUnits = async () => {
 
     for (let i=0; i < response.data.length; i++) {
         //get data for dynamic entry
+        
+        let unitAvailable = response.data[i].available;
+        let unitNumber = response.data[i].unitNumber;
+
+        if (unitAvailable) {
+        
         let unitStyle = await axios.get(`http://localhost:3001/unitStyle/${response.data[i].unitStyle_id}`);
         let unitRep = await axios.get(`http://localhost:3001/unitRep/${response.data[i].unitRep_id}`);
 
@@ -29,134 +38,109 @@ const populateUnits = async () => {
         let unitBaths = response.data[i].baths;
         let unitSqFt = response.data[i].sqFt;
         let unitLocation = response.data[i].location;
-        let unitNumber = response.data[i].unitNumber;
         let unitDeposit = response.data[i].deposit;
         let unitUtilities = response.data[i].utilitiesAvailable;
         let unitParking = response.data[i].parkingSpaces;
         let unitPetRent = response.data[i].petRent;
         let unitPetPolicy = response.data[i].petPolicy;
         let unitDescription = response.data[i].description;
-        let unitAvailable = response.data[i].available;
+        
         let unitAcceptsPets = response.data[i].acceptsPets;
         let unitImgLink = response.data[i].unitImg;
 
         //define Div's to be created
         let newUnitDisplayDiv = document.createElement("div"); //unit container
-
         let newUnitImgDiv = document.createElement("div"); //unit img div container
         let newPetTag = document.createElement("div"); //unit img pet tag div
         let newUnitImg = document.createElement("img"); //unit img tag
-        
         let newUnitInfo = document.createElement("div"); //unit info div container
         let newUnitRent = document.createElement("div"); //unit rent amount div
         let newUnitStyle = document.createElement("div"); //unit style tag
+        let newUnitLine1Style = document.createElement("div"); //wrap around rent and style
         let newUnitBeds = document.createElement("div"); // unit beds
         let newUnitBedsIcon = document.createElement("div"); // unit beds icon
         let newUnitBaths = document.createElement("div"); //unit baths
         let newUnitBathsIcon = document.createElement("div"); //unit baths icon
         let newUnitSqFt = document.createElement("div"); //unit sqFt
         let newUnitSqFtIcon = document.createElement("div"); //unit sqFt icon
+        let newUnitLine2Style = document.createElement("div") //wrap around beds,baths, and sqFt
         let newAddressLocation = document.createElement("div"); //unit address location (i.e., E, W, or N)
         let newUnitNumber = document.createElement("div"); //unit number
         let newDepositPolicy = document.createElement("div"); //deposit policy
-        
-        let newAdditionalDetailsHeading = document.createElement("div"); //bolded in-between text
-        
         let newUtilitiesAvailable = document.createElement("div"); //utilities available
         let newParkingSpaces = document.createElement("div"); //parking spaces
-        
-        let newPetsHeading = document.createElement("div"); //Pets heading
         let newPetRent = document.createElement("div"); //Pets rent
         let newPetPolicy = document.createElement("div"); //Pet policy
         let newContactBlurb = document.createElement("div"); //contact text
+        let newContactBtn = document.createElement("a"); //contact text
 
-        console.log(unitImgLink)
-        
         //Add Content
         newPetTag.innerHTML = 'Pet Friendly';
         newUnitImg.setAttribute('src',`${unitImgLink}`);
-
         newUnitRent.innerHTML = `&dollar;${unitRent}&#47;mo`;
         newUnitStyle.innerHTML = `${unitStyleName}`;
-        newUnitBeds.innerHTML = `${unitBeds}bd`;
-        newUnitBaths.innerHTML = `${unitBaths}ba`;
-        newUnitSqFt.innerHTML = `${unitSqFt} sqft`;
-        newAddressLocation.innerHTML = `Hatley Rd ${unitLocation}`;
-        newUnitNumber.innerHTML =`Unit #${unitNumber}`;
+        newUnitBeds.innerHTML = `<strong>${unitBeds}</strong>bd`;
+        newUnitBaths.innerHTML = `<strong>${unitBaths}</strong>ba`;
+        newUnitSqFt.innerHTML = `<strong>${unitSqFt}</strong> sqft`;
+        newAddressLocation.innerHTML = `Hatley Rd <strong>${unitLocation}</strong>, Pinn City, CT`;
+        newUnitNumber.innerHTML =`<strong>Unit #</strong>${unitNumber}`;
         newDepositPolicy.innerHTML =`Deposit Policy: ${unitDeposit}`;
-        newParkingSpaces.innerHTML = `Parking Spaces: ${unitParking}`;
-        newPetRent.innerHTML = `&dollar;${unitPetRent}&#47;mo`;
-        newPetPolicy.innerHTML = `Pet Policy: ${unitPetPolicy}`;
-        newContactBlurb.innerHTML =`For more information, contact the unit representative ${unitRepName}`
-
-
+        newParkingSpaces.innerHTML = `<strong>Parking Spaces:</strong> ${unitParking}`;
+        newPetRent.innerHTML = `<strong>Pet Rent:</strong> &dollar;${unitPetRent}&#47;mo`;
+        newPetPolicy.innerHTML = `<strong>Pet Policy:</strong> ${unitPetPolicy}`;
+        newContactBlurb.innerHTML =`For more information, contact the unit representative <strong>${unitRepName}</strong>`
+        newUtilitiesAvailable.innerHTML =`<strong>Utilities: </strong>${unitUtilities}`
+        newContactBtn.innerHTML = 'Contact Unit Representative'
         //Add to DOM
-        unitDisplayHolder.append(newUnitImgDiv);
+        unitDisplayHolder.append(newUnitDisplayDiv);
+        newUnitDisplayDiv.classList.add('unitDisplay');
 
-        newUnitImgDiv.append(newPetTag);
+        newUnitDisplayDiv.append(newUnitImgDiv);
+        if (unitAcceptsPets) {
+            newUnitImgDiv.append(newPetTag);
+        } 
+        newUnitImgDiv.classList.add('unitImg');
         newPetTag.classList.add('petTag');
         newUnitImgDiv.append(newUnitImg);
-        
 
-        unitDisplayHolder.append(newUnitDisplayDiv);
-        
-        newUnitDisplayDiv.append(newUnitRent);
-        newUnitDisplayDiv.classList.add('unitDisplay');
-        newUnitDisplayDiv.append(newUnitStyle);
-        newUnitDisplayDiv.append(newUnitBeds);
-        newUnitDisplayDiv.append(newUnitBaths);
-        newUnitDisplayDiv.append(newUnitSqFt);
-        newUnitDisplayDiv.append(newAddressLocation);
-        newUnitDisplayDiv.append(newUnitNumber);
-        newUnitDisplayDiv.append(newDepositPolicy);
-        newUnitDisplayDiv.append(newParkingSpaces);
-        newUnitDisplayDiv.append(newPetRent);
-        newUnitDisplayDiv.append(newPetPolicy);
-        newUnitDisplayDiv.append(newContactBlurb);
+        newUnitDisplayDiv.append(newUnitInfo);
+        newUnitInfo.classList.add('unitInfo')
+        newUnitInfo.append(newUnitLine1Style);
+        newUnitLine1Style.classList.add('wrap')
+        newUnitLine1Style.append(newUnitRent);
+        newUnitRent.classList.add('unitRent');
+        newUnitLine1Style.append(newUnitStyle);
+        newUnitStyle.classList.add('unitStyleTag')
 
-        }   
+        newUnitInfo.append(newUnitLine2Style);
+        newUnitLine2Style.classList.add('wrap');
+        newUnitLine2Style.append(newUnitBeds);
+        newUnitLine2Style.append(newUnitBaths);
+        newUnitLine2Style.append(newUnitSqFt);
+
+        newUnitInfo.append(newUnitNumber);
+        newUnitInfo.append(newAddressLocation);
+        newUnitInfo.append(newDepositPolicy);
+        newUnitInfo.append(newParkingSpaces);
+        // newUnitInfo.append(newUtilitiesAvailable);
+        newUnitInfo.append(newPetRent);
+        newUnitInfo.append(newPetPolicy);
+        newUnitInfo.append(newContactBlurb);
+        newUnitInfo.append(newContactBtn)
+        newContactBtn.classList.add('btn');
+        newContactBtn.setAttribute('href','../pages/about.html')
+
+        } else {
+            console.log(`Not displaying unavailable unit # ${unitNumber}`);
+        }
+    } 
 }
 
-
-//     unitDisplayHolder.append(`
-//     <div class="unitDisplay">
-//     <div class="unitImg">
-//         <div class="petTag">Pet Friendly</div>
-//         <img src="https://morguefile.nyc3.cdn.digitaloceanspaces.com/imageData/public/files/o/o0o0xmods0o0o/preview/fldr_2008_11_28/file000658805796.jpg" alt="unit image">
-//     </div>
-
-//     <div class="unitInfo">
-//         <div class="unitRent">&dollar;<span class="dynamicData">2000</span>&#47;mo</div>
-//         <div class="styleTag"><span class="dynamicData">Bensy Cottage</span></div>
-//         <div class="unitBeds"><img src="#" alt="beds icon"/> <span class="dynamicData">2</span> bd</div>
-//         <div class="unitBaths"><img src="#" alt="baths icon"/> <span class="dynamicData">2</span> bd</div>
-//         <div class="unitSqFt"><img src="#" alt="sqft icon"/> <span class="dynamicData">950</span> sqft</div>
-//         <div class="Address">
-//             <img src="#" alt="location icon"/> Hatley Rd <span class="dynamicData">W</span>,
-//             Unit # <span class="dynamicData">1</span><br>
-//             Pinn City, CT
-//         </div>
-//         <div class="unitDeposit">Deposit Policy <span class="dynamicData">Policy Here</span> </div>
-//         <div>Additional Details:</div>
-//         <div class="unitUtilities">Utilities Available: <span class="dynamicData">Utilities Here</span></div>
-//         <div class="unitParking">Parking Spaces: <span class="dynamicData">1</span></div>
-//         <div>Pets:</div>
-//         <div class="petRent">&dollar;<span class="dynamicData">20</span>&#47;mo</div>
-//         <div class="petPolicy">Pet Policy:<span class="dynamicData">policy here</span></div>
-//         <div class="unitRep">
-//             For more information, contact the unit representative <span class="dynamicData">Robert P</span>
-//             <button>Get in touch</button>
-//         </div>
-//         <button class="showMore">See More</button>
-//     </div>
-// </div>
-// `
-//     )
-    
-//}
-document.body.onload = populateUnits;
+//document.body.onload = populateUnits;
+populateUnits()
 
 //Events
+navBar.style.display='none';
 navBtn.addEventListener('click', () => {
     if (navBar.style.display == 'none') {
         navBar.style.display = 'block';
@@ -164,5 +148,16 @@ navBtn.addEventListener('click', () => {
             navBar.style.display = 'none';
     } else {
         console.log('error with nav bar toggle!')
+    }
+});
+
+sortBar.style.display ='none';
+sortBtn.addEventListener('click', () => {
+    if (sortBar.style.display == 'none') {
+        sortBar.style.display = 'block';
+    } else if (sortBar.style.display == 'block') {
+        sortBar.style.display = 'none';
+    } else {
+        console.log('error with sort bar toggle!')
     }
 });
